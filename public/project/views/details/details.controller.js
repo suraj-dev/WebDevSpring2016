6be90eac2eca9ea422f09876546a03fcb57ad858
Suraj Nagaraj <suraj04@ccs.neu.edu>
2016-03-02 15:29:24 -0500

    Added footer

--- public/project/views/details/details.controller.js
+++ public/project/views/details/details.controller.js
@@ -5,11 +5,12 @@
         .controller("DetailsController", DetailsController);
 
     function DetailsController($scope, $rootScope, $sce) {
+        $scope.detailsbg = "detailsbg";
         for(var key in $rootScope.data.query.pages){
-            if($rootScope.data.query.pages.hasOwnProperty(key)){
+            if($rootScope.data.query.pages.hasOwnProperty(key)) {
                 $scope.location_name = $rootScope.data.query.pages[key].title;
                 $scope.location_content = $sce.trustAsHtml($rootScope.data.query.pages[key].extract);
             }
-        }
+          }
         }
 })();
