0ac9e18b846bd4e174a37eea2d71d1e1fefbc5b7
Suraj Nagaraj <suraj04@ccs.neu.edu>
2016-03-03 21:08:38 -0500

    Added yelp information

--- public/project/services/yelpapi.service.js
+++ public/project/services/yelpapi.service.js
@@ -20,21 +20,23 @@
 
         function request_yelp(name, callback) {
                     var method = 'GET';
-                    var url = 'http://api.yelp.com/v2/search';
+                    var url = 'http://api.yelp.com/v2/search?callback=JSON_CALLBACK';
                     var params = {
                         callback: 'angular.callbacks._0',
-                        location: 'San+Francisco',
+                        location: name,
+                        limit : 10,
+                        sort: 2,
                         oauth_consumer_key: 'j-Kgx6pccnY0mCsvijWBww', //Consumer Key
                         oauth_token: '_rZOi_LkB62ikZ8eF6MBKU-NwPrHha6v', //Token
                         oauth_signature_method: "HMAC-SHA1",
                         oauth_timestamp: new Date().getTime(),
-                        oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-                        term: 'food'
+                        oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
                     };
                     var consumerSecret = 'iwTJ65m5paNf7CG_HLKU7xAJw2w'; //Consumer Secret
                     var tokenSecret = 'kL1vObCNdFETxcJqOZ7o_G6b_tQ'; //Token Secret
                     var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
                     params['oauth_signature'] = signature;
+
                     $http.jsonp(url, {params: params})
                         .success(callback);
                 }
--- public/project/views/details/details.controller.js
+++ public/project/views/details/details.controller.js
@@ -19,19 +19,20 @@
                     if($rootScope.data.query.pages.hasOwnProperty(key)) {
                         $scope.location_name = $rootScope.data.query.pages[key].title;
                         $scope.location_content = $sce.trustAsHtml($rootScope.data.query.pages[key].extract);
-                     /*   /!*YelpAPIService.request_yelp("San Francisco", function(response){
-                           console.log("Hello");*!/
-                        });*/
                         LocationService.findTitleByID(pageid, function(response){
                             var pageinfo = response;
                             for(var key in pageinfo.query.pages) {
                                 if (pageinfo.query.pages.hasOwnProperty(key)) {
                                     pageTitle = pageinfo.query.pages[key].title;
                                     console.log(pageTitle);
-                                    $scope.imgSrc = $sce.trustAsResourceUrl("http://www.panoramio.com/wapi/template/photo_list.html?tag=" + pageTitle +"&amp;width=700&amp;height=500&amp;list_size=8&amp;position=bottom&amp;bgcolor=%2333")
+                                    $scope.imgSrc = $sce.trustAsResourceUrl("http://www.panoramio.com/wapi/template/photo_list.html?tag=" + pageTitle +"&amp;width=500&amp;height=500&amp;list_size=8&amp;position=bottom&amp;bgcolor=%2333")
                                     break;
                                 }
                             }
+                        });
+                        YelpAPIService.request_yelp("Boston", function(response){
+                            console.log(response);
+                            $rootScope.yelpInfo = response;
                         });
                         $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
                     }
