d836f7138b8bdb5de0d09f0555dc71a7b84a1d8f
Suraj Nagaraj <suraj04@ccs.neu.edu>
2016-03-19 11:14:38 -0400

    few changes to home page dependencies

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
 /*app.use(uiBootstrap());*/
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3002;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
