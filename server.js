291d785612ba90bf5ebb3cb8b83086d9e456296b
Suraj Nagaraj <suraj04@ccs.neu.edu>
2016-03-28 19:01:50 -0400

    added MongoDB catridge

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ var app = express();
 var mongoose = require('mongoose');
 var connectionString = 'mongodb://127.0.0.1:27017/formmaker';
 
-/*if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
-}*/
+}
 
 var db = mongoose.connect(connectionString);
+console.log(process.env);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
