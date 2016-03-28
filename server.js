f388318f195196512cbbfc322da3d45ec56dee90
Suraj Nagaraj <suraj04@ccs.neu.edu>
2016-03-28 18:29:14 -0400

    testing remote connection

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
-/*app.use(uiBootstrap());*/
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
