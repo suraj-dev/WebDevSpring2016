2d46d0ef96730e6d7ec58ab387ebe9f2c73fdc03
Suraj Nagaraj <suraj04@ccs.neu.edu>
2016-03-29 04:54:30 -0400

    added missing field model file

---
+++ public/assignment/server/models/field.model.js
@@ -0,0 +1,68 @@
+var q = require("q");
+
+module.exports = function (db, mongoose, formModel) {
+
+    var FieldSchema = require('./field.schema.server.js')(mongoose);
+
+    var FormModel = formModel.getMongooseModel();
+
+    var api = {
+        findFieldsByFormId: findFieldsByFormId,
+        findFieldById: findFieldById,
+        deleteFieldById: deleteFieldById,
+        createField: createField
+        /*updateFieldById: updateFieldById*/
+    };
+
+    return api;
+
+    function findFieldsByFormId(formId) {
+        return FormModel.findById(formId).select("fields");
+    }
+
+    function findFieldById(fieldId, formId) {
+        return FormModel
+            .findById(formId)
+            .then(
+                function(form){
+                    return form.fields._id(fieldId);
+                }
+            );
+    }
+
+    function deleteFieldById(fieldId, formId) {
+       return FormModel
+            .findById(formId)
+            .then(
+                function(form){
+                    form.fields._id(fieldId).remove();
+                    return FormModel.save();
+                }
+            );
+    }
+
+    function createField(field, formId) {
+        return FormModel.findById(formId)
+            .then(
+                function(form) {
+                    form.fields.push(field);
+                    return form.save();
+                }
+            );
+    }
+
+    /*function updateFieldById(fieldId, field, formId) {
+
+        for (var u in mock) {
+            if (mock[u]._id === formId) {
+                for (var v in mock[u].fields) {
+                    if (v._id === fieldId) {
+                        v.label = field.label;
+                        v.type = field.type;
+                        v.placeholder = field.placeholder;
+                    }
+                }
+            }
+        }
+    }*/
+};
